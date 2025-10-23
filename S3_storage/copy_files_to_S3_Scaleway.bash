385c449b3112a8d812f06e612191183131c381df
Romain Burulci <burulciromain@gmail.com>
2025-10-23 20:11:10 +0000

    Ajout du code pour exporter les données utiles sur un bucket S3

---
+++ S3_storage/copy_files_to_S3_Scaleway.bash
@@ -0,0 +1,3 @@
+aws --endpoint-url https://s3.fr-par.scw.cloud s3 cp articles_pca.csv s3://your-s3-bucket/export_directory/articles_pca.csv
+aws --endpoint-url https://s3.fr-par.scw.cloud s3 cp all_clicks.csv s3://your-s3-bucket/export_directory/all_clicks.csv
+aws --endpoint-url https://s3.fr-par.scw.cloud s3 cp top5_neighbors_faiss.csv s3://your-s3-bucket/export_directory/top5_neighbors_faiss.csv
