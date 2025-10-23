caebf181093e0e7b1575b4a74fcb19c134f519d8
Romain Burulci <burulciromain@gmail.com>
2025-10-23 19:55:01 +0000

    Ajout du nécessaire pour déployer les fonctions de manière automatique sur Scaleway

---
+++ functions/deploy_functions.bash
@@ -0,0 +1,2 @@
+scw function deploy name=<FUNCTION_NAME> namespace-id=<NAMESPACE_ID> runtime=<YOUR_RUNTIME> zip-file=zips/function_compute_offline_cache.zip
+scw function deploy name=<FUNCTION_NAME> namespace-id=<NAMESPACE_ID> runtime=<YOUR_RUNTIME> zip-file=zips/function_compute_online_cache.zip
